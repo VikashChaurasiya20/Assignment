e6e180d6e46d1b3fbfb0a3e26332342434536f5f
VikashChaurasiya20 <116479356+VikashChaurasiya20@users.noreply.github.com>
2025-01-10 00:05:57 +0530

    domProject

---
+++ coading Challange/index.js
@@ -0,0 +1,31 @@
+function addNumber(a,b){
+    return a+b
+}
+console.log(addNumber(5,9));
+
+const multiplyNumber = (num1,num2)=>{
+return num1 * num2
+}
+
+console.log(multiplyNumber(4,5));
+
+if(true){
+    console.log(var1);
+    console.log(var2);
+    console.log(var3);
+
+   var var1 = "Hello";
+   let var2 = "PW";
+   const var3 = "Family";
+
+   console.log(var1);
+    console.log(var2);
+    console.log(var3);
+}
+
+if(true){
+    console.log(x);
+
+    let x = "Hello"
+    console.log(x);
+}
